4856812c0a386b299b0133d31e2ba813a2c02b6a
Utku Genç <Utku-Genc>
2024-04-09 19:06:23 +0300

    7-Movie Details Component

--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, input } from '@angular/core';
 import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'movie-detail',
@@ -7,6 +9,21 @@ import { Movie } from '../movie';
   styleUrl: './movie-detail.component.css'
 })
 export class MovieDetailComponent {
-@Input() movie!: Movie;
+  @Input() movie!: Movie;
+
+  constructor(
+    private movieService: MovieService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.getMovie()
+  }
+  getMovie(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.movieService.getMovie(id).subscribe(movie => this.movie = movie)
+  }
 
 }
--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -9,10 +9,17 @@ import { LoggingService } from './logging.service';
 })
 export class MovieService {
 
-  constructor(private loggingService:LoggingService) { }
+  constructor(private loggingService: LoggingService) { }
 
   getMovies(): Observable<Movie[]> {
     this.loggingService.add('MovieService: listing movies')
     return of(Movies);
   }
+
+  getMovie(id: number): Observable<Movie> {
+    this.loggingService.add("MovieService: get detail by id: " + id);
+    const foundMovie = Movies.find(movie => movie.id === id);
+    return of(foundMovie!); // Null kontrolü kaldırıldı
+  }
+  
 }
