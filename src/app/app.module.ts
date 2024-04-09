759757ab6a7d0ac740cd373e096537d6aa412cef
Utku Genç <Utku-Genc>
2024-04-09 16:20:03 +0300

    4-Service Create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { FormsModule } from '@angular/forms';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MoviesComponent,
-    MovieComponent
+    MovieComponent,
+    MovieDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/movie.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Movie } from './movie';
+import { Movies } from './movies.datasource';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor() { }
+
+  getMovies(): Movie[] {
+    return Movies;
+  }
+}
